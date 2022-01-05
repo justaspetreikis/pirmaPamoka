a4edb6d6bd580002750871b710ed6a7699710efc
justaspetreikis <petreikisjustas@gmail.com>
2022-01-05 15:16:24 +0200

    Created new application

---
+++ Pirma pamoka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pirma_pamoka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
